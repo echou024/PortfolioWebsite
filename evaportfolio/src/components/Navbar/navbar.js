473d88068952830971b0b0abd1ad9aa5df265875
Eva Choudhary <echou024@ucr.edu>
2024-05-21 13:33:03 -0700

    Navbar progress

--- evaportfolio/src/components/Navbar/navbar.js
+++ evaportfolio/src/components/Navbar/navbar.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './navbar.css'
+import Elogo from '../../assets/Elogo.png'
+import {Link} from 'react-scroll' 
+
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="navbar">
+        <img src= {Elogo} alt="Logo" className='logo'/>
+        <div className='desktopMenu'> 
+            <Link className='desktopMenuListItem'>Home</Link>
+            <Link className='desktopMenuListItem'>About Me</Link>
+            <Link className='desktopMenuListItem'>Skills</Link>
+            <Link className='desktopMenuListItem'>Portfolio</Link>
+        </div>
+        <button className="desktopMenuBtn">
+            <img src="" alt="" className='desktopMenuImg' />Let's Connect
+        </button>
+    </nav>
   )
 }
 
